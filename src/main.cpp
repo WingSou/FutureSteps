f5c0ea7904dfd77fcf4fc73f4e69ed1a2b18c352
R.komatsu <rkc_tw_st@yahoo.co.jp>
2015-11-05 18:38:39 +0900

    Appクラスの追加
    シェアードポインタの追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,23 @@
-﻿//
-// アプリ雛形
-// 
-// VisualStudioでもコンパイルできるようにするため
-// ソースコードをUTF-8(BOM付き)で用意:D
-//
-
-// Frameworkの全てをインクルード
+﻿
 #include "lib/framework.hpp"
+#include "Window.h"
+#include "App.h"
 
+std::shared_ptr<AppEnv> App::env;
 
-// アプリのウインドウサイズ
-enum Window {
-  WIDTH  = 512,
-  HEIGHT = 512
-};
-
-
-// 
-// メインプログラム
-// 
 int main() {
-  // アプリウインドウを生成
-  // GLFWとOpenGLの初期化
-  // TIPS:色々な型が依存しているので、最初に生成する事
-  AppEnv env(Window::WIDTH, Window::HEIGHT);
+ 
+	App::env =
+		std::make_shared<AppEnv>(WIDTH, HEIGHT, false, true);
 
-  while (env.isOpen()) {
-    // 開始
-    env.begin();
-    
-    // 
-    // アプリの処理をここに書く
-    // 
-    
-    // 終了
-    env.end();
+  while (App::env->isOpen()) {
+
+	  App::env->begin();
+
+
+
+
+
+	  App::env->end();
   }
 }
